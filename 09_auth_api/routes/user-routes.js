7a01e740409d78e1e7347c829f109d0ca62ebfb4
Halvaya <danikbodnar@gmail.com>
2023-06-27 21:45:04 +0300

    Add data validation

--- 09_auth_api/routes/user-routes.js
+++ 09_auth_api/routes/user-routes.js
@@ -2,69 +2,83 @@ import { Router } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { verifyToken } from "../middleware/jwtVerify.js";
+import { body, validationResult } from "express-validator";
 
 const userRouter = new Router();
 
 const createUserRouter = (supabase) => {
   const JWT_SECRET = process.env.JWT_SECRET;
-  console.log(JWT_SECRET);
-  userRouter.post("/register", async (req, res) => {
-    console.log(JWT_SECRET);
+  userRouter.post(
+    "/register",
+    [
+      body("email").isEmail().withMessage("Invalid email"),
+      body("password")
+        .isLength({ min: 6 })
+        .withMessage("Password must be at least 6 characters long"),
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-    const { email, password } = req.body;
+      const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Email and password are required" });
+      if (!email || !password) {
+        return res
+          .status(400)
+          .json({ message: "Email and password are required" });
+      }
+
+      try {
+        const { data: existingUser, error: fetchError } = await supabase
+          .from("users")
+          .select()
+          .eq("email", email)
+          .maybeSingle();
+
+        if (fetchError) {
+          console.error(fetchError);
+          return res.sendStatus(500);
+        }
+
+        if (existingUser) {
+          return res
+            .status(409)
+            .json({ message: "Email is already registered" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const { error: insertError } = await supabase
+          .from("users")
+          .insert([{ email, password: hashedPassword }])
+          .single();
+
+        if (insertError) {
+          console.error(insertError);
+          return res.sendStatus(500);
+        }
+
+        const accessToken = jwt.sign({ email }, JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        const refreshToken = jwt.sign({ email }, JWT_SECRET);
+
+        res.status(200).json({
+          success: true,
+          data: {
+            email,
+            accessToken,
+            refreshToken,
+          },
+        });
+      } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+      }
     }
-
-    try {
-      const { data: existingUser, error: fetchError } = await supabase
-        .from("users")
-        .select()
-        .eq("email", email)
-        .maybeSingle();
-
-      if (fetchError) {
-        console.error(fetchError);
-        return res.sendStatus(500);
-      }
-
-      if (existingUser) {
-        return res.status(409).json({ message: "Email is already registered" });
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 12);
-
-      const { error: insertError } = await supabase
-        .from("users")
-        .insert([{ email, password: hashedPassword }])
-        .single();
-
-      if (insertError) {
-        console.error(insertError);
-        return res.sendStatus(500);
-      }
-
-      const accessToken = jwt.sign({ email }, JWT_SECRET, {
-        expiresIn: "1h",
-      });
-      const refreshToken = jwt.sign({ email }, JWT_SECRET);
-
-      res.status(200).json({
-        success: true,
-        data: {
-          email,
-          accessToken,
-          refreshToken,
-        },
-      });
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
-  });
+  );
 
   userRouter.post("/login", async (req, res) => {
     const { email, password } = req.body;
